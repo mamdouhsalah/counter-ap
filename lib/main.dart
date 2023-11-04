3928537a37a924f68d3104e3c05518bc15d25bbb
mamdouhsalah <mamdouhsalah631@gmail.com>
2023-11-04 17:24:30 +0200

    app

--- lib/main.dart
+++ lib/main.dart
@@ -1,105 +1,127 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const Myapp());
 }
 
-class CounterPage extends StatefulWidget {
-  const CounterPage({Key? key}) : super(key: key);
-
-  @override
-  State<CounterPage> createState() => _CounterPageState();
-}
-
-class _CounterPageState extends State<CounterPage> {
-  int count = 0;
-
-  void increment() {
-    setState(() {
-      count++;
-    });
-  }
-
-  void reset() {
-    setState(() {
-      count = 0;
-    });
-  }
-
-  void decrement() {
-    setState(() {
-      count--;
-    });
-  }
+class Myapp extends StatelessWidget {
+  const Myapp({super.key});
 
   @override
   Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData.light(useMaterial3: true),
+        debugShowCheckedModeBanner: false,
+        home: const MyHomePage());
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int count = 0;
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: reset,
-        child: const Icon(Icons.restore),
-      ),
       appBar: AppBar(
-        title: const Center(
-          child: Text('simple counter app'),
+        elevation: 7,
+        centerTitle: true,
+        title: const Text(
+          "Counter app",
+          style: TextStyle(color: Colors.black),
         ),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text(
-              'simple counter app',
-              style: TextStyle(fontSize: 22),
-            ),
-            const Text(
-              '                    ',
-              style: TextStyle(fontSize: 25),
-            ),
-             Container(
-              width: 80,
-              height: 80,
-              decoration: const BoxDecoration(
-                shape: BoxShape.circle,
-                color: Colors.blue,
-              ),
-              child: Center(
-                child: Text(
-                  '$count',
-                  style: const TextStyle(fontSize: 48),
-                ),
+            const SizedBox(
+              width: 300,
+              child: Text(
+                "simple counter app",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 20),
               ),
             ),
-            Row( 
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                FloatingActionButton(
-                  onPressed: increment,
-                  child: const Icon(Icons.add), 
-                ),
-                const SizedBox(width: 16), 
-                FloatingActionButton(
-                  onPressed: decrement,
-                  child: const Icon(Icons.remove),
-                ),
-              ],
+            const SizedBox(
+              height: 30,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Color.fromARGB(255, 168, 109, 178),
+                      blurRadius: 20,
+                    )
+                  ],
+                  color: const Color.fromARGB(255, 255, 255, 255),
+                  borderRadius: BorderRadius.circular(15)),
+              width: 280,
+              height: 300,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  CircleAvatar(
+                    radius: 50,
+                    child: Text(
+                      count.toString(),
+                      style: const TextStyle(fontSize: 55),
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      CircleAvatar(
+                        radius: 30,
+                        child: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              count++;
+                            });
+                          },
+                          icon: const Icon(
+                            Icons.add,
+                            size: 30,
+                          ),
+                        ),
+                      ),
+                      CircleAvatar(
+                        radius: 30,
+                        child: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              count--;
+                            });
+                          },
+                          icon: const Icon(
+                            Icons.remove,
+                            size: 30,
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
             ),
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        elevation: 10,
+        onPressed: () {
+          setState(() {
+            count = 0;
+          });
+        },
+        child: Icon(Icons.restore),
+      ),
     );
   }
 }
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Counter App',
-      home: CounterPage(),
-    );
-  }
-}
-
-
